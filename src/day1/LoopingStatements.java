7b88113a5b33e2b965bd69d2915779cccf1fb63f
Saranya <Saranya@172.16.247.18>
2025-08-06 14:59:17 +0530

    commit1

--- src/day1/LoopingStatements.java
+++ src/day1/LoopingStatements.java
@@ -1,10 +1,20 @@
 package day1;
 
 public class LoopingStatements {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello");
+	
+		public static void main(String[] args) 
+		{	
+			for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+			{	
+				System.out.print("Value of i:");
+				System.out.println(i);
+			}
+		}
 	}
 
-}
+
+	
+	
+	
+	
+
